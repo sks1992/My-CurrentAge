e1547b5386fd361475b59739fcc182032721ab31
sks1992 <sandeepSK1581992@gmail.com>
2021-03-31 21:00:12 +0530

    Complete Code

--- app/src/main/java/com/sandeep/mycurrentage/MainActivity.kt
+++ app/src/main/java/com/sandeep/mycurrentage/MainActivity.kt
@@ -2,10 +2,27 @@ package com.sandeep.mycurrentage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            Toast.makeText(this,"GetAge is Clicked",Toast.LENGTH_SHORT).show()
+            buttonClicked()
+
+        }
+    }
+    private fun buttonClicked(){
+        val userDOB:Int = Integer.parseInt(etDOB.text.toString())
+        val currentAge:Int = Calendar.getInstance().get(Calendar.YEAR)
+        val userAge:Int = currentAge-userDOB
+
+        tvShowDate.text = "Your age is $userAge years"
     }
 }
